dbb373b775f3df85e0f731d637e4bc7547b1fb16
David Dairo <david.adewoyin@baruchmail.cuny.edu>
2016-12-06 03:54:37 -0500

    Minor package.json tweaks

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {routing} from './app.routing';
+import { routing } from './app.routing';
 
 import { AppComponent }  from './app.component';
 
