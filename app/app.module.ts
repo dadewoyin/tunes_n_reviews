2191ac079100dceacca311b8fd823db63aa7d92f
David Dairo <david.adewoyin@baruchmail.cuny.edu>
2016-12-06 03:45:05 -0500

    Set up routing

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,13 +1,16 @@
 import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {routing} from './app.routing';
 
 import { AppComponent }  from './app.component';
+
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { AboutComponent } from './components/about/about.component';
 import { SearchComponent } from './components/search/search.component';
 
+
 @NgModule({
-  imports:      [ BrowserModule ],
+  imports:      [ BrowserModule, routing ],
   declarations: [ AppComponent, 
   				  NavbarComponent, 
   				  AboutComponent, 
